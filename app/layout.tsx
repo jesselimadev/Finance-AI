efe37205068ea17c6c53bf97a8d996880539b6ee
jesselimadev <170663907+jesselimadev@users.noreply.github.com>
2024-11-17 19:36:46 +0000

    chore: add prisma and database setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <h1>Seja bem vindo ao Layout!!!</h1>
         {children}
       </body>
     </html>
